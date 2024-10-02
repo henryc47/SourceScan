9c58e848412a499b32bf204b73d12f93912358d7
Henry Chadban <HenryJ.Chadban@gmail.com>
2024-10-02 22:26:29 +1000

    testing

--- test_script.py
+++ test_script.py
@@ -3,6 +3,8 @@ import sys
 def process_diff(diff):
     # Process the diff lines
     print("num lines =",len(diff),"lines")
+    for line in diff:
+        print(line)
 
 if __name__ == "__main__":
     # Read diff data from stdin
