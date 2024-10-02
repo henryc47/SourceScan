ca5d918cbb3b2b722613477b71e623915a7dd1aa
Henry Chadban <HenryJ.Chadban@gmail.com>
2024-10-02 22:28:04 +1000

    testing line spacing

--- test_script.py
+++ test_script.py
@@ -1,10 +1,12 @@
 import sys
 
 def process_diff(diff):
+#spacetesting
     # Process the diff lines
     print("num lines =",len(diff),"lines")
     for line in diff:
         print(line)
+
 
 if __name__ == "__main__":
     # Read diff data from stdin
