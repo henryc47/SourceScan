5c94f45e8e5879cc47034737fc477278f8902327
Henry Chadban <HenryJ.Chadban@gmail.com>
2024-11-03 18:46:37 +1100

    added in some wrapper code to handle moving files to the processing area

--- sourcescan.py
+++ sourcescan.py
@@ -1,5 +1,5 @@
 import sys
-
+import pickle #for storing objects
 
 def command_handler(arguments):
     if len(arguments)<=1:
@@ -10,7 +10,7 @@ def command_handler(arguments):
         extra_arguments = sys.argv[2:]
         if main_argument == "help":
             sourcescan_help()
-        elif main_argument == "scan_init":
+        elif main_argument == "scan-init":
             scan_init()
         elif main_argument == "scan-file":
             scan_file(extra_arguments)
@@ -44,29 +44,48 @@ def sourcescan_help():
 def scan_init():
     pass
 
-def scan_file(extra_arguments):
-    pass
-    #for file in extra_arguments:  
+#scan each of the provided filenames
+def scan_file(extra_arguments,clean=False):
+    for file in extra_arguments:
+        scan_single_file(file,clean)  
 
-
-def scan_all():
-    pass
-
+#scan all files in the provided directory, unless they are marked as not to be scanned
+def scan_all(clean=False):
+    files_to_scan = get_files_in_directory()
+    scan_file(files_to_scan,clean)
+    
+#just a wrapper for scan_file, with the clean argument set to True
 def scan_file_clean(extra_arguments):
-    pass
+    scan_file(extra_arguments,True)
 
-def scan_all_clean():
-    pass
+#just a wrapper for scan_all, with the clean argument set to True
+def scan_all_clean(ex):
+    scan_all(True)
 
+#reset the list of false positives
 def false_positive_reset():
     pass
 
+#view the current list of false positives
 def view_marked_false_positives():
     pass
 
-def scan_single_file(filename):
-    try 
+#scan an individual file
+def scan_single_file(filename,clean):
+    try:
+        file = open(filename,mode="r") 
+    except:
+        print(filename," could not be found in the current directory",file=sys.stderr)
+        return
+    #TODO, actually scan the file
+    print("scanning ",filename)
 
-#
+
+#find and return a list of filepaths in the directory
+def get_files_in_directory():
+    files = []
+    #TODO, later, actually get the filenames
+    return files
+
 if __name__ == "__main__":
     command_handler(sys.argv)
