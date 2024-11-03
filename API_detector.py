420f651bc45694ae71a1d9852e7032e46a661668
Henry Chadban <HenryJ.Chadban@gmail.com>
2024-11-04 00:49:15 +1100

    made the scanning code technique agnostic

--- API_detector.py
+++ API_detector.py
@@ -4,3 +4,5 @@ def detect_in_line(line):
         return True,line[20:]
     else:
         return False,None
+
+detection_message = "WARNING : Possible API_key detected ->"
--- sourcescan.py
+++ sourcescan.py
@@ -6,21 +6,30 @@ import API_detector
 
 class CommandHandler():
     def __init__(self):
+        self.line_scan_types = [API_detector] #currently available scan types
         #load the known false-positive patterns from file
         if os.path.exists("sourcescan_patterns.pkl"):
             with open("sourcescan_patterns.pkl", "rb") as file:
                 self.known_patterns = pickle.load(file)
         #if no file storing known false-positive patterns from file
         else:
-            self.known_patterns = set() #set storing all the known false-positive patterns for this project        
-    
+            self.known_patterns = set() #set storing all the known false-positive patterns for this project   
+        #load the known blocked file types from file
+        if os.path.exists("sourcescan_config.pkl"):
+            with open("sourcescan_blocked_files.pkl", "rb") as file:
+                self.blocked_files = pickle.load(file)
+        else:
+            self.blocked_files = set()
+             
     #store known false positive patterns on program exit
     def store_known_patterns(self):
         with open("sourcescan_patterns.pkl", "wb") as file:
             pickle.dump(self.known_patterns,file)
 
-
-
+    #store blocked file patterns on program exit
+    def store_block_file_types(self):
+        with open("sourcescan_blocked_files.pkl", "wb") as file:
+            pickle.dump(self.blocked_files,file)
 
     def run(self,arguments):
         if len(arguments)<=1:
@@ -45,37 +54,46 @@ class CommandHandler():
                 self.false_positive_reset()
             elif main_argument == "view-marked-false-positives":
                 self.view_marked_false_positives()
+            elif main_argument == "add-blocked-file-type":
+                self.add_blocked_file_pattern(extra_arguments)
+            elif main_argument == "remove-blocked-file-type":
+                self.remove_blocked_file_pattern(extra_arguments)
+            elif main_argument == "view-blocked-file-type":
+                self.view_blocked_file_patterns()
             else:
                 print(main_argument," is not a valid command")
                 sys.exit(1)
             #do stuff on file exit
             self.store_known_patterns()
+            self.store_block_file_types()
             sys.exit(0) #default exit without error
 
     def sourcescan_help(self):
         print("Valid commands for sourcescan are")
         print("help - provides a list of valid commands")
-        print("scan-init - setup rules for source scanning in this project")
+        print("init - setup rules for source scanning in this project")
         print("scan-file - scan a file for any security errors")
         print("scan-all - scan all files in the current directory")
         print("scan-file-clean - scan a file for any security errors, disregarding existing confirmed false positives")
         print("scan-all-clean - scan all files in the current directory, disregarding existing confirmed false positives")
         print("false-positive-reset - reset the list of false positives for this directory")
         print("view-marked-false-positives - view all the strings which have been marked as false positives")
-        sys.exit(0)
+        print("add-blocked-file-type - add a type of file to block")
+        print("remove-blocked-file-type - stop blocking a type of file")
+        print("view-blocked-file-type - print out all the file types that are currently blocked")
 
     #setup the configuration files for SourceScan
     def scan_init(self):
+        
         print("Welcome to the SourceScan Initialization Wizard")
-        if os.path.exists(".sourcescanconfig"):
+        if os.path.exists("sourcescanconfig.pkl"):
             user_input = input("You have already initialized SourceScan in this directory, Type y to reinitalize: ")
             if not user_input=="y":
                 print("SourceScan initialization cancelled")
                 return
         user_input = input("Do you want to prevent SourceScan from analyzing it's own configuration files? (Recommended) Type y to confirm : ")
         if user_input=="y":
-            pass
-            #TODO
+            self.blocked_files.add(".pkl")
         user_input = input("Do you want to add SourceScan configuration files to your .gitignore file (Recommended) Type y to confirm : ")
         if user_input=="y":
             self.update_gitignore()
@@ -83,14 +101,6 @@ class CommandHandler():
         if user_input=="y":
             pass
             #TODO
-        print("Do you want to add any other types of files to the list of files SourceScan will not process, type exit to exit")
-        while True:
-            user_input = input("type file extension (after the dot) here : ")
-            if user_input=="exit":
-                break
-            else:
-                #TODO 
-                pass
         print("Initialization complete")
 
     #scan each of the provided filenames
@@ -131,17 +141,18 @@ class CommandHandler():
         print("scanning ",filename)
         for line_number,line in enumerate(file):
             line = line.rstrip()
-            API_key_detected,dodgy_pattern = API_detector.detect_in_line(line)
-            if API_key_detected:
-                if not clean: #if we are "scanning" clean, we don't care if a pattern has been marked false positive before
-                    if dodgy_pattern in self.known_patterns: #no need to detect a pattern multiple times
-                        continue
-                print("WARNING : Possible API Key -> ",dodgy_pattern," Detected on line ",line_number,"-> ",line)
-                user_input = input("Type y to confirm that this is intentional and you are happy with this being in your source code: ")
-                if (user_input=="y"): #if it is intentional, store it in list of known false positive patterns
-                    self.known_patterns.add(dodgy_pattern)
-            else:
-                continue
+            for scan_type in self.line_scan_types:
+                detected,dodgy_pattern = scan_type.detect_in_line(line)
+                if detected:
+                    if not clean: #if we are "scanning" clean, we don't care if a pattern has been marked false positive before
+                        if dodgy_pattern in self.known_patterns: #no need to detect a pattern multiple times
+                            continue
+                    print(scan_type.detection_message,dodgy_pattern," Detected on line ",line_number,"-> ",line)
+                    user_input = input("Type y to confirm that this is intentional and you are happy with this being in your source code: ")
+                    if (user_input=="y"): #if it is intentional, store it in list of known false positive patterns
+                        self.known_patterns.add(dodgy_pattern)
+                else:
+                    continue
 
 
     #find and return a list of filepaths in the directory
@@ -154,10 +165,19 @@ class CommandHandler():
     def update_gitignore(self): #yes if you keep reinitalizing the file, these same patterns will keep being added to the gitignore which is kind of silly, but should be fairly harmless
         file = open('.gitignore','a')
         file.write("sourcescan_patterns.pkl\n")
-        file.write(".sourcescanconfig\n")
+        file.write("sourcescan_config.pkl\n")
 
-            
+    def add_blocked_file_pattern(self,patterns):
+        for pattern in patterns:
+            self.blocked_files.add(pattern)
 
+    def remove_blocked_file_pattern(self,patterns):
+        for pattern in patterns:
+            self.blocked_files.discard(pattern)
+
+    def view_blocked_file_patterns(self):
+        for pattern in self.block_files:
+            print(pattern)   
 
 
 if __name__ == "__main__":
